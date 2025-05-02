9710f86dfb1b35a164efeb6779e50232c1501b7a
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2025-05-02 14:19:11 +0330

    Server-side authentication access levels with getSession

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,5 +1,22 @@
+import { getSession } from "next-auth/react";
+
 function Dashboard() {
   return <div>Dashboard</div>;
 }
 
 export default Dashboard;
+
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/signin",
+        permanent: false,
+      },
+    };
+  }
+
+  return { props: { session } };
+}
